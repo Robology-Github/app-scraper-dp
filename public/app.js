7eb6a92b90868ee038150c942f8f13438fd39bcd
robogentlenam <121732750+robogentlenam@users.noreply.github.com>
2024-03-12 19:03:27 +0100

    Input page redesign + bug fixes

--- public/app.js
+++ public/app.js
@@ -1,11 +1,14 @@
-document.getElementById('searchForm').addEventListener('submit', async function(e) {
-    e.preventDefault();
-    const searchTerm = document.getElementById('searchTerm').value;
-    const numResults = document.getElementById('numResults').value;
-  
-    const response = await fetch(`/search?term=${encodeURIComponent(searchTerm)}&num=${encodeURIComponent(numResults)}`);
-    const data = await response.json();
-  
-    document.getElementById('results').textContent = JSON.stringify(data, null, 2);
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('searchForm').addEventListener('submit', async function(e) {
+        e.preventDefault();
+        const searchTerm = document.getElementById('searchTerm').value;
+        const numResults = document.getElementById('numResults').value;
+      
+        const response = await fetch(`/search?term=${encodeURIComponent(searchTerm)}&num=${encodeURIComponent(numResults)}`);
+        const data = await response.json();
+      
+        //document.getElementById('results').textContent = JSON.stringify(data, null, 2);
+      });
+      
   });
-  
